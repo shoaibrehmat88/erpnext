c3deca521262d2c81398879e26eb979738f8427e
Shoaib Rehmat <86551638+shoaibrehmat88@users.noreply.github.com>
2023-12-12 15:23:14 +0500

    button hide

--- erpnext/public/js/controllers/stock_controller.js
+++ erpnext/public/js/controllers/stock_controller.js
@@ -69,18 +69,18 @@ erpnext.stock.StockController = class StockController extends frappe.ui.form.Con
 	show_general_ledger() {
 		var me = this;
 		if(this.frm.doc.docstatus > 0) {
-			cur_frm.add_custom_button(__('Accounting Ledger'), function() {
-				frappe.route_options = {
-					voucher_no: me.frm.doc.name,
-					from_date: me.frm.doc.posting_date,
-					to_date: moment(me.frm.doc.modified).format('YYYY-MM-DD'),
-					company: me.frm.doc.company,
-					group_by: "Group by Voucher (Consolidated)",
-					show_cancelled_entries: me.frm.doc.docstatus === 2,
-					ignore_prepared_report: true
-				};
-				frappe.set_route("query-report", "General Ledger");
-			}, __("View"));
+			// cur_frm.add_custom_button(__('Accounting Ledger'), function() {
+			// 	frappe.route_options = {
+			// 		voucher_no: me.frm.doc.name,
+			// 		from_date: me.frm.doc.posting_date,
+			// 		to_date: moment(me.frm.doc.modified).format('YYYY-MM-DD'),
+			// 		company: me.frm.doc.company,
+			// 		group_by: "Group by Voucher (Consolidated)",
+			// 		show_cancelled_entries: me.frm.doc.docstatus === 2,
+			// 		ignore_prepared_report: true
+			// 	};
+			// 	frappe.set_route("query-report", "General Ledger");
+			// }, __("View"));
 		}
 	}
 };
