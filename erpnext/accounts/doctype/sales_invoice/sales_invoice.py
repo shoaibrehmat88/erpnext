b3d744d5b870d12f12f3e596e88ea771f8c64ddb
Shoaib Rehmat <86551638+shoaibrehmat88@users.noreply.github.com>
2023-12-14 22:51:17 +0500

    change in erpn

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -862,10 +862,10 @@ class SalesInvoice(SellingController):
 	def validate_pos(self):
 		if self.is_return:
 			invoice_total = self.rounded_total or self.grand_total
-			if flt(self.paid_amount) + flt(self.write_off_amount) - flt(invoice_total) > 1.0 / (
-				10.0 ** (self.precision("grand_total") + 1.0)
-			):
-				frappe.throw(_("Paid amount + Write Off Amount can not be greater than Grand Total"))
+			# if flt(self.paid_amount) + flt(self.write_off_amount) - flt(invoice_total) > 1.0 / (
+			# 	10.0 ** (self.precision("grand_total") + 1.0)
+			# ):
+			# 	frappe.throw(_("Paid amount + Write Off Amount can not be greater than Grand Total"))
 
 	def validate_item_code(self):
 		for d in self.get("items"):
